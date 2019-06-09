aaf019705b1133240aa9cfe5c0d8e30af22722ea
markwilde33 <markwilde29@gmail.com>
2019-06-09 11:30:02 +0100

    Welcome the user

--- Program.cs
+++ Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 
+// Namespace
 namespace NumberGuesser
 {
+    // Main Class
     class Program
     {
+        // Entry Point Method
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Set app vars
+            string appName = "Number Guesser";
+            string appVersion = "1.0.0";
+            string appAuthor = "Mark Wilde";
+
+            // Change text color
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            // Write out app info
+            Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
+
+            // Reset text color
+            Console.ResetColor();
+
+            // Ask users name
+            Console.WriteLine("What is your name?");
+
+            // Get user input
+            string input = Console.ReadLine();
+
+            Console.WriteLine("Aye up {0}, let's be aving you...", input);
         }
     }
 }
